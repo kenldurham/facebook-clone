69cf8ae4c8779d556139d5e4096bbc4cd4fd0cd8
Kenneth <kenldurham@gmail.com>
2021-10-08 16:13:00 -0700

    facebook update

---
+++ components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Image from 'next/image'
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from '@heroicons/react/solid'
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from '@heroicons/react/outline';
+
+function Header() {
+    return (
+        <div>
+           <h1>Header</h1> 
+           {/* Left*/}
+           <div>
+               <Image
+               src="http://links.papareact.com/5me" 
+               width={40}
+               height={40}
+               layout="fixed"
+               />
+               <div>
+                   
+                   <input type="text" placeholder="Search Facebook"/>
+               </div>
+           </div>
+           {/* center*/}
+           {/* right*/}
+        </div>
+    )
+}
+
+export default Header
